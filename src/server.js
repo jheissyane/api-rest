dbaf048b794e3eb34bc6f49b9d79f3f806c952e1
jheissyane <105369996+jheissyane@users.noreply.github.com>
2023-11-29 19:53:19 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect("", {
   useNewUrlParser: true,
