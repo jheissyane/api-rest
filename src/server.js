9fa4511c4371da2ae1000099bb558ee20564360b
Jheissyane <jheissyane@discente.ufg.br>
2023-11-29 18:29:01 -0300

    att

--- src/server.js
+++ src/server.js
@@ -1,35 +1,32 @@
 /* eslint-disable linebreak-style */
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = 3000;
 
-mongoose.connect(
-    'mongodb+srv://jheissyanekelly:Xt2hskd2fxgHRh3e@cluster0.kxaalez.mongodb.net/?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-);
+mongoose.connect("", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('connected', () => {
-  console.log('Conexão ao MongoDB estabelecida com sucesso!');
+db.on("connected", () => {
+  console.log("Conexão ao MongoDB estabelecida com sucesso!");
 });
 
-db.on('error', (err) => {
+db.on("error", (err) => {
   console.error(`Erro na conexão ao MongoDB: ${err}`);
 });
 
 app.use(bodyParser.json());
 
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server rodando na porta: ${PORT}`);
